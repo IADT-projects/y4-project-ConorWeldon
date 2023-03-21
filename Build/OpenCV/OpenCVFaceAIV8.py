91511d7bdef288adc959a88c73a8db226651f1b7
ConorWeldon <71635864+ConorWeldon@users.noreply.github.com>
2023-03-21 18:20:59 +0000

    OpenCVFaceAIV8
    My version 8 now detects smiles and neutrality using both your smile and your eyes.

--- Build/OpenCV/OpenCVFaceAIV8.py
+++ Build/OpenCV/OpenCVFaceAIV8.py
@@ -1,7 +1,8 @@
 import cv2
 
-# Load the pre-trained face and emotion detection models from OpenCV
+# Load the pre-trained face, eye and smile detection models from OpenCV
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 def recognize_emotion_and_face():
@@ -24,6 +25,16 @@ def recognize_emotion_and_face():
         # Capture a frame from the webcam video stream
         ret, frame = cap.read()
 
+        # Convert the frame to JPEG format
+        ret, image_data = cv2.imencode('.jpg', frame)
+
+        # Display the captured frame
+        cv2.imshow('Captured Frame', frame)
+
+        # Check for key presses
+        if cv2.waitKey(1) == ord('q'):
+            break
+
         # Process each detected face
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
@@ -35,28 +46,45 @@ def recognize_emotion_and_face():
             face_roi_gray = gray[y:y+h, x:x+w]
             face_roi_color = frame[y:y+h, x:x+w]
 
-            # Detect smiles in the face region
+            # Detect eyes in the face region
+            eyes = eye_cascade.detectMultiScale(face_roi_gray, scaleFactor=1.1, minNeighbors=5)
+
+            # Detect smile in the face region
             smiles = smile_cascade.detectMultiScale(face_roi_gray, scaleFactor=1.1, minNeighbors=5)
 
-            # Process each detected smile
-            for (ex, ey, ew, eh) in smiles:
+            # Check if both eyes and smile are detected
+            if len(eyes) > 1 and len(smiles) > 0:
+                # Draw rectangles around the eyes
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(face_roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
+
                 # Draw a rectangle around the smile
-                cv2.rectangle(face_roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+                for (sx, sy, sw, sh) in smiles:
+                    cv2.rectangle(face_roi_color, (sx, sy), (sx+sw, sy+sh), (255, 0, 0), 2)
 
-                # Determine the emotion label based on the detected smile
-                emotion_label = "Happy"
+                # Calculate the confidence percentage
+                confidence_percent = round((len(eyes) + len(smiles)) / 10 * 100)
 
-                # Print the emotion label to the console
-                print(emotion_label)
+                # Determine the emotion label based on the detected face region
+                if confidence_percent > 60:
+                    emotion_label = "Happy"
+                else:
+                    emotion_label = "Neutral"
 
-        # Display the processed frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+                # Display the emotion label and confidence percentage next to the recognized face
+                text = f"{emotion_label} ({confidence_percent}%)"
+                cv2.putText(frame, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    # Release the resources used by the webcam and close the window
+        # Show the processed frame
+        cv2.imshow('Processed Frame', frame)
+
+    # Release the webcam and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+def main():
+    # Call the recognize_emotion_and_face function
     recognize_emotion_and_face()
+
+if __name__ == '__main__':
+    main()
